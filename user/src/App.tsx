3aa7743ae92ff227bf7c72458ab6eacb62f18d3b
Aniruddha Chandra <aniruddha.282003@gmail.com>
2025-12-27 13:59:31 +0530

    remove unused imports

--- user/src/App.tsx
+++ user/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { SyncProvider } from './context/SyncContext';
 import { StatusBar } from './components/StatusBar';
 import { EntryForm } from './components/EntryForm';
--- user/src/components/StatusBar.tsx
+++ user/src/components/StatusBar.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { useSync } from '../context/SyncContext';
 
 export const StatusBar = () => {
